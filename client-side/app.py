e9a9650667786e3db7df5f9674836f9579aec449
psgrkcwhospitalmanagement <22bai037@psgrkcw.ac.in>
2025-03-09 15:01:44 +0530

    app.py

--- client-side/app.py
+++ client-side/app.py
@@ -18,7 +18,7 @@ def submit():
     model = request.files['model']
 
     ## replace the url from the ngrok url provided on the notebook on server.
-    url = "http://e793-34-123-73-186.ngrok-free.app/api/transform"
+    url = "https://a104-103-114-210-172.ngrok-free.app/api/transform"
     print("sending")
     response = requests.post(url=url, files={"cloth":cloth.stream, "model":model.stream})
     op = Image.open(BytesIO(response.content))
